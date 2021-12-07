c2666f12e0b63250f61f9971038b27dabee33b62
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2021-12-07 19:43:22 -0300

    termina segundo exercicio

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -9,10 +9,27 @@ primeiroFilhoDoFilho.innerText = "Primeiro filho do filho";
 // 4. Acesse o primeiroFilho a partir de pai .
 const primeiroFilho = pai.firstChild;
 // 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
-const primeiroFilho = elemento.parentNode.firstChild;
+elemento.parentNode.firstChild;
 // 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
 const atencao = elemento.nextSibling;
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
 const terceiroFilho = elemento.nextElementSibling;
 // 8. Agora acesse o terceiroFilho a partir de pai .
-const terceiroFilho = pai.lastChild.previousSibling;
+pai.lastChild.previousSibling;
+// 1. Crie um irmão para elementoOndeVoceEsta .
+let irmaoTexto = "Irmão";
+let irmaoElemento = document.createElement("p");
+irmaoElemento.innerText = irmaoTexto;
+elemento.parentNode.appendChild(irmaoElemento);
+// 2. Crie um filho para elementoOndeVoceEsta .
+let filhoTexto = "Filho";
+let filhoElemento = document.createElement("p");
+filhoElemento.innerText = filhoTexto;
+elemento.appendChild(filhoElemento);
+// 3. Crie um filho para primeiroFilhoDoFilho .
+let filhoDoPrimeiroFilhoDoFilhoTexto = "Filho do primeiro filho do filho";
+let filhoDoPrimeiroFilhoDoFilhoElemento = document.createElement("p");
+filhoDoPrimeiroFilhoDoFilhoElemento.innerText = filhoDoPrimeiroFilhoDoFilhoTexto;
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilhoElemento);
+// 4. A partir desse filho criado, acesse terceiroFilho .
+let terceiroFilhoAPartirDeFilhoCriado = filhoDoPrimeiroFilhoDoFilhoElemento.parentNode.parentNode.nextElementSibling;
