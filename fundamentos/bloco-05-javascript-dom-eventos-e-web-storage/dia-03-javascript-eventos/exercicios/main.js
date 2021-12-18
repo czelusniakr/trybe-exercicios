97fe537d6eac3ca838a5964cc92b3e485479a903
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2021-12-18 17:19:00 -0300

    corrige conteúdo dos arquivos css e js

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/main.js
@@ -1,70 +1,39 @@
-body {
-  align-items: center;
-  background-color: #333;
-  color: white;
-  display: flex;
-  flex-flow: column wrap;
-  font-family: Montserrat, sans-serif;
-  height: 100vh;
-  justify-content: center;
-  margin: 0;
-  padding: 0;
-  width: 100vw;
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+
+
+// 1. Copie esse arquivo e edite apenas ele;
+// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
+// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
+// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portifólio?
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
 }
 
-.container {
-  display: flex;
-  flex-flow: row wrap;
-}
+firstLi.addEventListener('dblclick', resetText);
 
-.container li {
-  align-items: center;
-  display: flex;
-  flex-flow: column wrap;
-  height: 200px;
-  justify-content: center;
-  margin: 2px;
-  padding: 15px;
-  text-align: center;
-  transition: all 0.25s ease;
-  width: 200px;
-}
-
-h1 {
-  font-size: 48px;
-  color: #2fc18c;
-}
-
-.container li:nth-of-type(1) {
-  background-color: #2fc18c;
-  color: #333;
-}
-
-.container li:nth-of-type(2) {
-  background-color: #006dfb;
-}
-
-.container li:nth-of-type(3) {
-  background-color: #41197f;
-}
-
-input {
-  border: 1px solid #333;
-  margin: 10px 0;
-  padding: 5px;
-  text-align: center;
-  width: 200px;
-}
-
-input:hover {
-  border: 1px solid #2fc18c;
-}
-
-input:focus {
-  border: 1px solid #006dfb;
-  outline: 2px solid #006dfb;
-}
-
-.tech {
-  transform: translateY(-20px);
-}
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
