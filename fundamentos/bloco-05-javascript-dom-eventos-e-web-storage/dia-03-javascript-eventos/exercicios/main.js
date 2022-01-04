d9d44b7089e95d6702af2992dd5cc34444236c11
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-01-04 16:01:37 -0300

    termina exercicios

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const firstLi = document.getElementById('first-li');
 const secondLi = document.getElementById('second-li');
 const thirdLi = document.getElementById('third-li');
@@ -37,10 +38,75 @@ function resetText(event) {
   event.target.innerText = 'Opção reiniciada';
   // O event possui várias propriedades, porém a mais usada é o event.target,
   // que retorna o objeto que disparou o evento.
+=======
+body {
+  align-items: center;
+  background-color: #333;
+  color: white;
+  display: flex;
+  flex-flow: column wrap;
+  font-family: Montserrat, sans-serif;
+  height: 100vh;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+>>>>>>> parent of 97fe537 (corrige conteúdo dos arquivos css e js)
 }
 
-firstLi.addEventListener('dblclick', resetText);
+.container {
+  display: flex;
+  flex-flow: row wrap;
+}
 
-// Não precisa passar o parâmetro dentro da callback resetText. O próprio
-// navegador fará esse trabalho por você, não é legal? Desse jeito, o
-// event.target na nossa função retornará o objeto 'firstLi'.
+.container li {
+  align-items: center;
+  display: flex;
+  flex-flow: column wrap;
+  height: 200px;
+  justify-content: center;
+  margin: 2px;
+  padding: 15px;
+  text-align: center;
+  transition: all 0.25s ease;
+  width: 200px;
+}
+
+h1 {
+  font-size: 48px;
+  color: #2fc18c;
+}
+
+.container li:nth-of-type(1) {
+  background-color: #2fc18c;
+  color: #333;
+}
+
+.container li:nth-of-type(2) {
+  background-color: #006dfb;
+}
+
+.container li:nth-of-type(3) {
+  background-color: #41197f;
+}
+
+input {
+  border: 1px solid #333;
+  margin: 10px 0;
+  padding: 5px;
+  text-align: center;
+  width: 200px;
+}
+
+input:hover {
+  border: 1px solid #2fc18c;
+}
+
+input:focus {
+  border: 1px solid #006dfb;
+  outline: 2px solid #006dfb;
+}
+
+.tech {
+  transform: translateY(-20px);
+}
