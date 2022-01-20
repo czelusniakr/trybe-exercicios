34aff51cce131cd5796282f2753c6d4dbd45860a
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-01-20 19:55:23 -0300

    termina primeiro script

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios/script.js
@@ -0,0 +1,17 @@
+function acordando() {
+  return "Acordando!!";
+}
+
+function cafe() {
+  return "Bora tomar café!!";
+}
+
+function dormir() {
+  return "Partiu dormir!!";
+}
+
+function doingThings(funcao) {
+  console.log(funcao);
+}
+
+doingThings(cafe());
