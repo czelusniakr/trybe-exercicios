31d40f496b071c971a4756db8605e3264cd3f850
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-03-10 16:19:36 -0300

    termina quarto js

--- fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script2.js
+++ fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script2.js
@@ -1,3 +1,5 @@
+// Referência: https://app.betrybe.com/course/fundamentals/introducao-a-javascript-es6-e-testes-unitarios/javascript-es6-parte-1/solutions/81c644e5-1f1b-467a-89a2-909bcc625c8a/gabarito-dos-exercicios/a28fa634-61a2-440a-a32a-8fd1aca67fc9?use_case=calendar
+
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortOddsAndEvens = () => {
--- fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script4.js
+++ fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script4.js
@@ -1,10 +1,15 @@
 // Referência: https://app.betrybe.com/course/fundamentals/introducao-a-javascript-es6-e-testes-unitarios/javascript-es6-parte-1/solutions/81c644e5-1f1b-467a-89a2-909bcc625c8a/gabarito-dos-exercicios/a28fa634-61a2-440a-a32a-8fd1aca67fc9?use_case=calendar
 
-const factorial = number => {
-  let result = 1;
+const longestWord = text => {
+  let wordArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
 
-  for (let index = 2; index <= number; index += 1) {
-    result *= index;
+  for (const word of wordArray) {
+    if (word.length > maxLength) {
+      maxLength = word.length;
+      result = word;
+    }
   }
 
   return result;
