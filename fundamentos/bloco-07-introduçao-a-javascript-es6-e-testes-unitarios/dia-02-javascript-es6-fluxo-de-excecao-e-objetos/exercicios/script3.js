47dcbd7af33e5700aca5bc2b05c2d80db75ae516
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-03-15 00:47:29 -0300

    termina terceiro exercicio

--- fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicios/script3.js
+++ fundamentos/bloco-07-introduçao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/exercicios/script3.js
@@ -29,3 +29,23 @@ const tamanhoObjeto = objeto => Object.keys(objeto).length;
 const valoresObjeto = objeto => Object.values(objeto);
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+const totalEstudantes = objeto => {
+  let total = 0;
+  const array = Object.keys(objeto);
+  for (index in array) {
+    total += objeto[array[index]].numeroEstudantes;
+  }
+  return total;
+};
+
+const valorPosicao = (objeto, posicao) => Object.values(objeto)[posicao];
+
+const chaveValor = (objeto, chave, valor) => {
+  const array = Object.entries(objeto);
+  let isEqual = false;
+  for (let index in array) {
+    if (array[index][0] === chave && array[index][1] === valor) isEqual = true;
+  }
+  return isEqual;
+};
