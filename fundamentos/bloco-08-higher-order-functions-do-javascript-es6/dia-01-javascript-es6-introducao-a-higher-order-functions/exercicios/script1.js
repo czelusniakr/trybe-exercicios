b9117fdb8b9197afda328ef8c031c9ab26b9b183
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-03-15 23:43:53 -0300

    termina primeiro exercicio

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios/script1.js
@@ -0,0 +1,11 @@
+const acordando = () => "Acordando!!";
+
+const cafe = () => "Bora tomar café!!";
+
+const dormir = () => "Partiu dormir!!";
+
+const doingThings = funcao => {
+  console.log(funcao);
+};
+
+doingThings(cafe());
