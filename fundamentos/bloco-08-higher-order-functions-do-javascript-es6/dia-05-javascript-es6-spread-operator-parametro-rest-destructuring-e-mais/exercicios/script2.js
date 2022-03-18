0f9b9fc0b297e3a26b9c8be95a783afcf9a231fd
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-03-18 20:41:18 -0300

    termina segundo exercicio

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios/script2.js
@@ -0,0 +1,19 @@
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const userInfo = {
+  ...user,
+  ...jobInfos,
+};
+
+console.log(userInfo);
