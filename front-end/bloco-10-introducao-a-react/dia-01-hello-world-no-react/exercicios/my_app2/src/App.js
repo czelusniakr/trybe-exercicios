7dc171ae377bfa51ba6a9b44ac512f539618a949
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-05-06 20:21:55 -0300

    termina terceiro exercicio

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicios/my_app2/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Componente from './Component';
+
+function App() {
+  return <Componente />;
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicios/my_app2/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Componente extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rafaela Czelusniak</h1>
+        <p>Uma pessoa que ama programar.</p>
+      </div>
+    );
+  };
+}
+
+export default Componente;
