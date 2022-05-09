90e1a10194196207950890d7e649ba425651e697
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-05-09 13:38:02 -0300

    termina quarto exercicio

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicios/my_app4/src/App.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    )
+  };
+}
+
+export default App;
