4e438fe11edf4ffae217c3c76b08ec4cdad3ad4f
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-06-01 10:51:27 -0300

    termina exercicio 7

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/exercicios/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/exercicios/fancy-buttons/src/App.js
@@ -3,22 +3,38 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super()
+    
     this.handleButtonOne = this.handleButtonOne.bind(this);
     this.handleButtonTwo = this.handleButtonTwo.bind(this);
     this.handleButtonThree = this.handleButtonThree.bind(this);
+
+    this.stage = {
+      clicksBtnOne: 0,
+      clicksBtnTwo: 0,
+      clicksBtnThree: 0,
+    };
   }
 
   handleButtonOne() {
+    this.setState((prevState) => ({
+      clicksBtnOne: prevState.clicksBtnOne + 1,
+    }));
     console.log(this);
     console.log('Clicou no botão 1!');
   }
   
   handleButtonTwo() {
+    this.setState((prevState) => ({
+      clicksBtnTwo: prevState.clicksBtnTwo + 1,
+    }));
     console.log(this);
     console.log('Clicou no botão 2!');
   }
   
   handleButtonThree() {
+    this.setState(({ clicksBtnThree }) => ({
+      clicksBtnThree: clicksBtnThree + 1,
+    }));
     console.log(this);
     console.log('Clicou no botão 3!');
   }
@@ -26,9 +42,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={ this.handleButtonOne }>Botão 1</button>
-        <button onClick={ this.handleButtonTwo }>Botão 2</button>
-        <button onClick={ this.handleButtonThree }>Botão 3</button>
+        <button onClick={ this.handleButtonOne }>Botão 1 | Count = {this.state.clicksBtnOne}</button>
+        <button onClick={ this.handleButtonTwo }>Botão 2 | Count = {this.state.clicksBtnTwo}</button>
+        <button onClick={ this.handleButtonThree }>Botão 3 | Count = {this.state.clicksBtnThree}</button>
       </div>
     );
   }
