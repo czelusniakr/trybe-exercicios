c2e47cfa1f6a51ffe12b9cc049f2b4d5b0906754
Rafaela Czelusniak <czelusniakrafaela@gmail.com>
2022-06-21 11:16:24 -0300

    termina exercicios

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/exercicios/my-form-2.0/src/Form.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PersonalForm from './PersonalForm'
+import ProfessionalForm from './ProfessionalForm'
+
+class Form extends React.Component {
+  render() {
+    const { sendForm, resetForm, changeHandler, currentState, onBlurHandler } = this.props;
+
+    return (
+      <form>
+        <PersonalForm
+          changeHandler={ changeHandler }
+          onBlurHandler={ onBlurHandler }
+          currentState={ currentState }
+        />
+        <ProfessionalForm changeHandler={ changeHandler } />
+        <input
+          type="button"
+          onClick={ sendForm }
+          value="Enviar"
+        />
+        <input
+          type="reset"
+          onClick={ resetForm }
+          value="Limpar"
+        />
+      </form>
+    );
+  }
+}
+
+export default Form;
